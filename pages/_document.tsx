1e9c42f642b306d0ec1a73891bf26756c37d2aab
amer <amer.h@orient-tv.net>
2023-09-16 01:09:35 +0300

    fix font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,6 +7,9 @@ export default function Document() {
   return (
     <Html lang="ar" >
       <Head />
+      <link rel="preconnect" href="https://fonts.googleapis.com"/>
+      <link rel="preconnect" href="https://fonts.gstatic.com"/>
+      <link href="https://fonts.googleapis.com/css2?family=Gulzar&family=Harmattan:wght@500&display=swap" rel="stylesheet"></link>
       <body dir='rtl'>
         <Main />
         <NextScript />
