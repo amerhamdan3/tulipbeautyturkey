89b4252b078ced50d7817328de00ab40b8d773f8
amer <amer.h@orient-tv.net>
2023-10-03 13:09:32 +0300

    edit font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,7 +9,7 @@ export default function Document() {
       <Head />
       <link rel="preconnect" href="https://fonts.googleapis.com"/>
       <link rel="preconnect" href="https://fonts.gstatic.com"/>
-      <link href="https://fonts.googleapis.com/css2?family=Gulzar&family=Harmattan:wght@500&display=swap" rel="stylesheet"></link>
+      <link href="https://fonts.googleapis.com/css2?family=Cairo&family=Gulzar&family=Harmattan:wght@500&family=Rubik&display=swap" rel="stylesheet"/>
       <body dir='rtl'>
         <Main />
         <NextScript />
